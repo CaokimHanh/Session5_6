20b2eb201bdd726adf3fd39408eedef9f1ba8d3d
CaokimHanh <106530492+CaokimHanh@users.noreply.github.com>
2022-06-14 16:24:46 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int co1, co2;
+	for(co1 = 1; co2 <= 100; co1++)
+	{
+printf("Enter %d co2: ", co2);
+scanf("%d", &co2);
+if(co2==100)
+break;
+	
+	}
+	
+	return 0;
+}
